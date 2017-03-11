ec69fc7b0c1d0bfc6ceda693cc2814b0f6fee2a2
pjh <pengjiahao321@163.com>
2017-03-11 13:03:22 +0800

    新增文件

---
+++ src/main/java/main/Spider.java
@@ -0,0 +1,34 @@
+package main;
+
+import download.HttpClientDownloader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import parse.Html;
+import parse.Page;
+import utils.Method;
+import utils.Request;
+
+import java.util.List;
+
+/**
+ * 爬虫的入口
+ * Created by pjh on 2017/1/22.
+ */
+public class Spider {
+
+    private static final Logger logger = LoggerFactory.getLogger(Spider.class);
+
+
+
+    public static void main(String[] args){
+
+        Request statrRequest = new Request("http://news.baidu.com/", Method.GET);
+        HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+        Html html = new Html(httpClientDownloader.getHtml(statrRequest));
+        Page page = new Page(statrRequest.getUrl(), html);
+        page.setTargetRequests(html.linksForRegx("[a-zA-z]+://[^\\s]*"), Method.GET);
+
+        
+        logger.info("目标队列的大小： {}", page.getTargetRequests().size());
+    }
+}
---
+++ src/main/java/parse/Html.java
@@ -0,0 +1,41 @@
+package parse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Created by pjh on 2017/2/10.
+ */
+public class Html {
+
+    private static final Logger logger = LoggerFactory.getLogger(Html.class);
+
+    private Document document;
+
+    public Html(String text){
+        try{
+            this.document = Jsoup.parse(text);
+        }catch (Exception e){
+            logger.error("Jsoup解析文本出错: {}", e.toString());
+        }
+    }
+
+    public Document getDocument(){
+        return this.document;
+    }
+
+    public List<String> allLinks(){
+        Select select = new Select();
+        return select.links(this.document);
+    }
+
+    public List<String> linksForRegx(String regx){
+        Select select = new Select();
+        return select.links(this.document, regx);
+    }
+}
